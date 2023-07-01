9eb5345bfd2c89e2daec426ec08d9bd1fc2244bc
Rian Emilio <rianemilioalves@gmail.com>
2023-07-01 07:51:31 -0300

    commit 4

--- sistemabancario.py
+++ sistemabancario.py
@@ -18,29 +18,48 @@ while True:
 
     if opcao == "d":
         valor = float(input("Informe o Valor do Depósito: \n"))
+        
         if valor > 0:
             saldo += valor
             extrato += f"Depósito: R$ {valor:.2f} \n"
             print("Valor Depositado com Sucesso")
+        
         else:
             print("O Valor Informado é Inválido")
 
     elif opcao == "s":
+        
         valor = float(input("Informe o Valor do Saque: \n"))
-        if saldo >= valor:
-            if valor > 0 and valor < 501 and numero_de_saques != limite_de_saques:
-                numero_de_saques += 1
-                extrato += f"Saque: R$ {valor:.2f} \n"
-                saldo -= valor
-                print("Valor Sacado com Sucesso")
-            else: 
-                print("O Valor Informado é Inválido \n")
+        
+        if numero_de_saques == limite_de_saques:
+        
+            print("O Número Máximo de Saques foi alcançado \n")
+        
         else:
-            print("Valor Insuficiente na conta \n")
-    
+            
+            if saldo < valor:
+                print("Valor Insuficiente na conta \n") 
+            
+            else: 
+                
+                if valor > 0 and valor < 501:
+                    numero_de_saques += 1
+                    extrato += f"Saque: R$ {valor:.2f} \n"
+                    saldo -= valor
+                    print("Valor Sacado com Sucesso")
+                
+                else: 
+                    print("O Valor Informado é Inválido \n")
+
     elif opcao == "e":
-        print(extrato) 
+        print("\n--------Extrato--------")
+        print("Não Foram Realizadas Movimentações" if not extrato else extrato)
+        print(f"\nSaldo: R$ {saldo:.2f}")
+        print("-------------------------")
 
     elif opcao == "q":
         print("O Programa Foi Encerrado \n")
         break
+    
+    else:
+        print("O Valor Informado é Inválido")
