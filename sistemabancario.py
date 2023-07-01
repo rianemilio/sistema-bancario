e5bf4420df4e3a16aac64a633f6664f9bda16b1a
Rian Emilio <rianemilioalves@gmail.com>
2023-07-01 07:14:11 -0300

    commit 3

--- sistemabancario.py
+++ sistemabancario.py
@@ -20,22 +20,27 @@ while True:
         valor = float(input("Informe o Valor do Depósito: \n"))
         if valor > 0:
             saldo += valor
-            extrato += f"Depósito: R$ {valor:.2f}\n"
+            extrato += f"Depósito: R$ {valor:.2f} \n"
+            print("Valor Depositado com Sucesso")
         else:
             print("O Valor Informado é Inválido")
 
     elif opcao == "s":
         valor = float(input("Informe o Valor do Saque: \n"))
-        if valor > 0 and valor < 501 and numero_de_saques != limite_de_saques:
-            numero_de_saques += 1
-            extrato += f"Saque: R$ {valor:.2f}\n"
-            saldo -= valor
-        else: 
-            print("O Valor Informado é Inválido")
+        if saldo >= valor:
+            if valor > 0 and valor < 501 and numero_de_saques != limite_de_saques:
+                numero_de_saques += 1
+                extrato += f"Saque: R$ {valor:.2f} \n"
+                saldo -= valor
+                print("Valor Sacado com Sucesso")
+            else: 
+                print("O Valor Informado é Inválido \n")
+        else:
+            print("Valor Insuficiente na conta \n")
     
     elif opcao == "e":
-        print(extrato)
+        print(extrato) 
 
     elif opcao == "q":
-        print("O Programa Foi Encerrado")
+        print("O Programa Foi Encerrado \n")
         break
